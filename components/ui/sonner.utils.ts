b6cad909bba41b0740812b11ad6c417d29cb255d
OnofreBR <jaime.onofree@gmail.com>
2025-10-07 18:22:51 -0300

    refactor: isolate utils from Sonner for Fast Refresh

---
+++ components/ui/sonner.utils.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import * as React from "react";
+import { Toaster as Sonner, toast } from "sonner";
+
+export type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+export { toast };
